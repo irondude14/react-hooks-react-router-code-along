b9ce61204b8b943bac71576ec863e628d41e5fc0
Vladyslav <vkadyrov@protonmail.com>
2022-12-19 11:59:50 -0500

    commit tree 5e25fbfb939c090e14ec0f3c137d4f513d2d5c08

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home!</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+export default Home;
